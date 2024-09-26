3e9b121dca0c9de23b48d8e33516868163517db8
brochristopher <cwbrownatx@gmail.com>
2024-09-26 13:23:12 -0500

    Update 20. Valid Parentheses.py
    Can move the short-circuit to the top, because we know to break out if there is an odd length for s

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -2,12 +2,12 @@ class Solution:
     def isValid(self, s: str) -> bool:
         valid_parens = ["()","[]","{}"]
 
+        if len(s) % 2 != 0:
+                    return (False)
+
         for i in s: 
             for j in valid_parens:
                 s = s.replace(j, "")
 
                 if len(s) == 0:
                     return(True)
-                
-                if len(s) % 2 != 0:
-                    return (False)
